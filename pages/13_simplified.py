1af71c753e1c50b0ab1e6ed6fa624dfd5e132e6e
Michael Wong <michael.wong@dxc.com>
2024-07-20 19:31:00 +0800

    Update 13_simplified.py

--- pages/13_simplified.py
+++ pages/13_simplified.py
@@ -9,7 +9,13 @@ import matplotlib.pyplot as plt
 
 # hard-coded values
 vdf_filepath = r"dataSources/ScrapedOutputFiles/(Roy) data validation.xlsx" 
-vdf = pd.read_excel(vdf_filepath, dtype = str)
+topN = 3
+section = 'Section'
+division = 'Division'
+group = 'Group'
+Class = 'Class'
+subclass = 'Sub-class'
+level = subclass
 
 pd.set_option('display.max_columns', None)
 
@@ -22,11 +28,11 @@ pd.set_option('display.max_columns', None)
 # 4. 'Class'
 # 5. 'Subclass'
 
-level_input = st.selectbox(
-    "Classification Model Level",
-    ("Division", "Group", 'Class', 'Subclass'))
+# level_input = st.selectbox(
+#     "Classification Model Level",
+#     ("Division", "Group", 'Class', 'Subclass'))
 
-level = level_input if level_input else 'Class'
+# level = level_input if level_input else 'Class'
 ####################################################################################################
 
 df_detailed_def = pd.read_excel(r"dataSources/DoS/ssic2020-detailed-definitions.xlsx", skiprows=4)
@@ -82,8 +88,6 @@ ssic_5['Division'] = ssic_5['SSIC 2020'].astype(str).str[:2]
 ssic_5['Group'] = ssic_5['SSIC 2020'].astype(str).str[:3]
 ssic_5['Class'] = ssic_5['SSIC 2020'].astype(str).str[:4]
 
-import pdb;pdb.set_trace()
-
 # join ssic_n Hierarhical Layer Tables (Section, Division, Group, Class, Sub-Class)
 ssic_df = pd.merge(ssic_5, ssic_1[['Section', 'Section Title', 'Section, 2 digit code']], on='Section, 2 digit code', how='left')
 ssic_df = pd.merge(ssic_df, ssic_2[['Division', 'Division Title']], on='Division', how='left')
@@ -92,11 +96,11 @@ ssic_df = pd.merge(ssic_df, ssic_4[['Class', 'Class Title']], on='Class', how='l
 
 # mapping
 level_map = {
-    'Section': ('Section', ssic_df.iloc[:, [0, 1, 9, 10, 11, 12, 13]].drop_duplicates(), ssic_1.iloc[:, [0, 1]].drop_duplicates().reset_index(drop=True), "nusebacra/ssicsync_section_classifier", ssic_1),
-    'Division': ('Division', ssic_df.iloc[:, [0, 1, 6, 10, 11, 12, 13]].drop_duplicates(), ssic_2.iloc[:, [0, 1]].drop_duplicates().reset_index(drop=True), "nusebacra/ssicsync_division_classifier", ssic_2),
-    'Group': ('Group', ssic_df.iloc[:, [0, 1, 7, 10, 11, 12, 13]].drop_duplicates(), ssic_3.iloc[:, [0, 1]].drop_duplicates().reset_index(drop=True), "nusebacra/ssicsync_group_classifier", ssic_3),
-    'Class': ('Class', ssic_df.iloc[:, [0, 1, 8, 10, 11, 12, 13]].drop_duplicates(), ssic_4.iloc[:, [0, 1]].drop_duplicates().reset_index(drop=True), "nusebacra/ssicsync_class_classifier", ssic_4),
-    'Subclass': ('SSIC 2020', ssic_df.iloc[:, [0, 1, 9, 10, 11, 12, 13]].drop_duplicates(), ssic_5.iloc[:, [0, 1]].drop_duplicates().reset_index(drop=True), "nusebacra/ssicsync_subclass_classifier", ssic_5)
+    section: ('Section', ssic_df.iloc[:, [0, 1, 9, 10, 11, 12, 13]].drop_duplicates(), ssic_1.iloc[:, [0, 1]].drop_duplicates().reset_index(drop=True), "nusebacra/ssicsync_section_classifier", ssic_1),
+    division: ('Division', ssic_df.iloc[:, [0, 1, 6, 10, 11, 12, 13]].drop_duplicates(), ssic_2.iloc[:, [0, 1]].drop_duplicates().reset_index(drop=True), "nusebacra/ssicsync_division_classifier", ssic_2),
+    group: ('Group', ssic_df.iloc[:, [0, 1, 7, 10, 11, 12, 13]].drop_duplicates(), ssic_3.iloc[:, [0, 1]].drop_duplicates().reset_index(drop=True), "nusebacra/ssicsync_group_classifier", ssic_3),
+    Class: ('Class', ssic_df.iloc[:, [0, 1, 8, 10, 11, 12, 13]].drop_duplicates(), ssic_4.iloc[:, [0, 1]].drop_duplicates().reset_index(drop=True), "nusebacra/ssicsync_class_classifier", ssic_4),
+    subclass: ('SSIC 2020', ssic_df.iloc[:, [0, 1, 9, 10, 11, 12, 13]].drop_duplicates(), ssic_5.iloc[:, [0, 1]].drop_duplicates().reset_index(drop=True), "nusebacra/ssicsync_subclass_classifier", ssic_5)
 }
 
 # Get the values for a and b based on the lvl_train
@@ -107,8 +111,6 @@ lvl_train_title = lvl_train + " Title"
 df_prep = ssic_df[[lvl_train, 'Detailed Definitions']]
 df_prep['encoded_cat'] = df_prep[lvl_train].astype('category').cat.codes
 df_prep = df_prep[[lvl_train, 'encoded_cat']].drop_duplicates()
-
-
 
 # load model directly from huggingface
 tokenizer = AutoTokenizer.from_pretrained(model)
@@ -159,6 +161,7 @@ def predict_text(text, top_n=15):
 predictions = []
 
 # Iterate over each row of the DataFrame and apply the prediction function
+vdf = pd.read_excel(vdf_filepath, dtype = str)
 for idx, row in vdf.iterrows():
     text = row['Notes Page Content'] # Specify Notes Page Content2
     result = predict_text(text)
@@ -212,24 +215,30 @@ def check_alpha_in_list(row, N, m1, m2, p):
     else:
         return 'N'
 
-Section_N = st.text_input('Top N Section:', '')
-Division_N = st.text_input('Top N Division:', '')
-Group_N = st.text_input('Top N Group:', '')
-Class_N = st.text_input('Top N Class:', '')
-Subclass_N = st.text_input('Top N Subclass:', '')
+# Section_N = st.text_input('Top N Section:', '')
+# Division_N = st.text_input('Top N Division:', '')
+# Group_N = st.text_input('Top N Group:', '')
+# Class_N = st.text_input('Top N Class:', '')
+# Subclass_N = st.text_input('Top N Subclass:', '')
 
-# Set default values if inputs are blank
-Section_N = int(Section_N) if Section_N else 3
-Division_N = int(Division_N) if Division_N else 3
-Group_N = int(Group_N) if Group_N else 3
-Class_N = int(Class_N) if Class_N else 3
-Subclass_N = int(Subclass_N) if Subclass_N else 3
+# # Set default values if inputs are blank
+# Section_N = int(Section_N) if Section_N else 3
+# Division_N = int(Division_N) if Division_N else 3
+# Group_N = int(Group_N) if Group_N else 3
+# Class_N = int(Class_N) if Class_N else 3
+# Subclass_N = int(Subclass_N) if Subclass_N else 3
 
-Section_prediction_df['Within Top N (Section)'] = Section_prediction_df.apply(check_alpha_in_list, axis=1, N=Section_N, m1 = 'm_Section', m2 = 'm_Section2', p = 'p_Section')
-Division_prediction_df['Within Top N (Division)'] = Division_prediction_df.apply(check_alpha_in_list, axis=1, N=Division_N, m1 = 'm_Division', m2 = 'm_Division2', p = 'p_Division')
-Group_prediction_df['Within Top N (Group)'] = Group_prediction_df.apply(check_alpha_in_list, axis=1, N=Group_N, m1 = 'm_Group', m2 = 'm_Group2', p = 'p_Group')
-Class_prediction_df['Within Top N (Class)'] = Class_prediction_df.apply(check_alpha_in_list, axis=1, N=Class_N, m1 = 'm_Class', m2 = 'm_Class2', p = 'p_Class')
-Subclass_prediction_df['Within Top N (Sub-class)'] = Subclass_prediction_df.apply(check_alpha_in_list, axis=1, N=Subclass_N, m1 = 'm_Sub-class', m2 = 'm_Sub-class2', p = 'p_Sub-class')
+# Section_prediction_df['Within Top N (Section)'] = Section_prediction_df.apply(check_alpha_in_list, axis=1, N=Section_N, m1 = 'm_Section', m2 = 'm_Section2', p = 'p_Section')
+# Division_prediction_df['Within Top N (Division)'] = Division_prediction_df.apply(check_alpha_in_list, axis=1, N=Division_N, m1 = 'm_Division', m2 = 'm_Division2', p = 'p_Division')
+# Group_prediction_df['Within Top N (Group)'] = Group_prediction_df.apply(check_alpha_in_list, axis=1, N=Group_N, m1 = 'm_Group', m2 = 'm_Group2', p = 'p_Group')
+# Class_prediction_df['Within Top N (Class)'] = Class_prediction_df.apply(check_alpha_in_list, axis=1, N=Class_N, m1 = 'm_Class', m2 = 'm_Class2', p = 'p_Class')
+# Subclass_prediction_df['Within Top N (Sub-class)'] = Subclass_prediction_df.apply(check_alpha_in_list, axis=1, N=Subclass_N, m1 = 'm_Sub-class', m2 = 'm_Sub-class2', p = 'p_Sub-class')
+
+Section_prediction_df[section] = Section_prediction_df.apply(check_alpha_in_list, axis=1, N=topN, m1 = 'm_Section', m2 = 'm_Section2', p = 'p_Section')
+Division_prediction_df[division] = Division_prediction_df.apply(check_alpha_in_list, axis=1, N=topN, m1 = 'm_Division', m2 = 'm_Division2', p = 'p_Division')
+Group_prediction_df[group] = Group_prediction_df.apply(check_alpha_in_list, axis=1, N=topN, m1 = 'm_Group', m2 = 'm_Group2', p = 'p_Group')
+Class_prediction_df[Class] = Class_prediction_df.apply(check_alpha_in_list, axis=1, N=topN, m1 = 'm_Class', m2 = 'm_Class2', p = 'p_Class')
+Subclass_prediction_df[subclass] = Subclass_prediction_df.apply(check_alpha_in_list, axis=1, N=topN, m1 = 'm_Sub-class', m2 = 'm_Sub-class2', p = 'p_Sub-class')
 
 dfs = [
     Section_prediction_df,
@@ -244,13 +253,7 @@ merged_df = dfs[0]  # Start with the first DataFrame
 for df in dfs[1:]:
     merged_df = pd.merge(merged_df, df, on=['entity_name', 'ssic_code', 'ssic_code2', 'Notes Page Content', 'Notes Page Content2'], how='outer')
 
-columns_to_include = [
-    'Within Top N (Section)',
-    'Within Top N (Division)',
-    'Within Top N (Group)',
-    'Within Top N (Class)',
-    'Within Top N (Sub-class)'
-]
+columns_to_include = [section, division, group, Class, subclass]
 
 # Filter the DataFrame to include only the specified columns
 filtered_df = merged_df[columns_to_include]
@@ -262,87 +265,167 @@ for column in filtered_df.columns:
     percentages[column] = counts.reindex(['Y', 'N']).fillna(0)
 
 # Create a DataFrame from percentages
-percentages_df = pd.DataFrame(percentages).round(2)
-
+percentages_df = pd.DataFrame(percentages).round(1).reset_index(drop = True)
+percentages_df = percentages_df[percentages_df.index == 0]
 
 # Transpose the DataFrame for vertical plotting
-percentages_df_transposed = percentages_df.transpose()
+percentages_df_transposed = percentages_df.transpose().reset_index(drop = False)
+categories = percentages_df_transposed['index'].to_list()
+values = percentages_df_transposed[0].to_list()
 
-# Plotting
+# Create horizontal bar chart
 fig, ax = plt.subplots(figsize=(10, 6))
+bars = ax.barh(categories, values, color='skyblue')
+# ax.set_xlabel('Percentage')
+# ax.set_ylabel('Categories')
+ax.set_title('Accuracy')
+ax.set_xlim(0, 100)  # Assuming the percentage is between 0 and 100
 
-# Plotting the bars vertically
-bars1 = ax.bar(percentages_df_transposed.index, percentages_df_transposed['Y'], color='green', label='Y')
-bars2 = ax.bar(percentages_df_transposed.index, percentages_df_transposed['N'], bottom=percentages_df_transposed['Y'], color='orange', label='N')
-
-# Customizing the plot
-ax.set_title('Percentage of Y and N Values')
-ax.set_xlabel('Categories')
-ax.set_xticklabels([
-    f'Within Top {Section_N} (Section)', 
-    f'Within Top {Division_N} (Division)', 
-    f'Within Top {Group_N} (Group)', 
-    f'Within Top {Class_N} (Class)', 
-    f'Within Top {Subclass_N} (Sub-class)'
-], rotation=45, ha='right')
-ax.set_ylabel('Percentage')
-ax.set_ylim(0, 100)
-ax.legend()
+# Remove right and top spines
+ax.spines[['right', 'top']].set_visible(False)
 
 # Adding data labels
-ax.bar_label(bars1, fmt='%.2f%%')
-ax.bar_label(bars2, fmt='%.2f%%', label_type='center')
+for bar in bars:
+    ax.annotate(f'{bar.get_width()}%', 
+                xy=(bar.get_width(), bar.get_y() + bar.get_height() / 2),
+                xytext=(5, 0),  # 5 points offset
+                textcoords='offset points',
+                ha='left', va='center')
 
 # Adjust layout
 plt.tight_layout()
+
+# Display plot in Streamlit
+st.pyplot(fig)
+
+df_display = {
+    section: Section_prediction_df,
+    division: Division_prediction_df,
+    group: Group_prediction_df,
+    Class: Class_prediction_df,
+    subclass: Subclass_prediction_df
+}
+
+# Streamlit selectbox for user input
+level_input = st.selectbox(
+    "Level of correct classification",
+    (section, division, group, Class, subclass)
+)
+level = level_input if level_input else section
+
+levelDisplay_df = df_display[level]
+levelDisplay_df.rename(columns = {levelDisplay_df.columns[-1]: 'classification'}, inplace = True)
+correctWrongClassification_df = levelDisplay_df[['entity_name', 'classification']]
+
+# Display df with text wrapping and no truncation
+st.dataframe(
+    correctWrongClassification_df.style.set_properties(**{
+        'white-space': 'pre-wrap',
+        'overflow-wrap': 'break-word',
+    })
+)
+
+companies_tuple = tuple(levelDisplay_df.entity_name)
+
+companies_input = st.selectbox(
+    "List of Companies",
+    companies_tuple)
+
+def capitalize_sentence(text):
+    # Split the text into sentences
+    sentences = text.split('. ')
+    # Capitalize the first letter of each sentence
+    sentences = [sentence[0].upper() + sentence[1:].lower() if sentence else '' for sentence in sentences]
+    # Join the sentences back into a single string
+    return '. '.join(sentences)
+
+ssic_input = levelDisplay_df[levelDisplay_df.entity_name == companies_input].reset_index(drop = True).ssic_code[0]
+ssic2_input = levelDisplay_df[levelDisplay_df.entity_name == companies_input].reset_index(drop = True).ssic_code2[0]
+content_input = capitalize_sentence(levelDisplay_df[levelDisplay_df.entity_name == companies_input].reset_index(drop = True)['Notes Page Content'][0])
+content2_input = capitalize_sentence(levelDisplay_df[levelDisplay_df.entity_name == companies_input].reset_index(drop = True)['Notes Page Content2'][0])
+
+sectionTitle_input = capitalize_sentence(ssic_df[ssic_df['SSIC 2020'] == ssic_input].reset_index(drop = True)['Section Title'][0])
+divisionTitle_input = capitalize_sentence(ssic_df[ssic_df['SSIC 2020'] == ssic_input].reset_index(drop = True)['Division Title'][0])
+groupTitle_input = capitalize_sentence(ssic_df[ssic_df['SSIC 2020'] == ssic_input].reset_index(drop = True)['Group Title'][0])
+classTitle_input = capitalize_sentence(ssic_df[ssic_df['SSIC 2020'] == ssic_input].reset_index(drop = True)['Class Title'][0])
+subclassTitle_input = capitalize_sentence(ssic_df[ssic_df['SSIC 2020'] == ssic_input].reset_index(drop = True)['Detailed Definitions'][0])
+details_display = {
+    section: sectionTitle_input,
+    division: divisionTitle_input,
+    group: groupTitle_input,
+    Class: classTitle_input,
+    subclass: subclassTitle_input
+}
+details_input = details_display[level]
+
+sectionTitle2_input = capitalize_sentence(ssic_df[ssic_df['SSIC 2020'] == ssic2_input].reset_index(drop = True)['Section Title'][0])
+divisionTitle2_input = capitalize_sentence(ssic_df[ssic_df['SSIC 2020'] == ssic2_input].reset_index(drop = True)['Division Title'][0])
+groupTitle2_input = capitalize_sentence(ssic_df[ssic_df['SSIC 2020'] == ssic2_input].reset_index(drop = True)['Group Title'][0])
+classTitle2_input = capitalize_sentence(ssic_df[ssic_df['SSIC 2020'] == ssic2_input].reset_index(drop = True)['Class Title'][0])
+subclassTitle2_input = capitalize_sentence(ssic_df[ssic_df['SSIC 2020'] == ssic2_input].reset_index(drop = True)['Detailed Definitions'][0])
+details_display2 = {
+    section: sectionTitle2_input,
+    division: divisionTitle2_input,
+    group: groupTitle2_input,
+    Class: classTitle2_input,
+    subclass: subclassTitle2_input
+}
+details2_input = details_display2[level]
+
+st.header('Company SSIC Details')
+st.subheader('Company Name:')
+st.text(companies_input)
+
+st.subheader('1st SSIC:')
+st.text(ssic_input)
+st.subheader('1st SSIC definition:')
+st.text(details_input)
+st.subheader('Company 1st description:') # TODO how come got 2 descriptions?
+st.text(content_input)
+
+st.subheader('2nd SSIC:')
+st.text(ssic2_input)
+st.subheader('2nd SSIC definition:')
+st.text(details2_input)
+st.subheader('Company 2nd description:') # TODO how come got 2 descriptions?
+st.text(content2_input)
+
+# level_input = st.selectbox(
+#     "Classification Model Level",
+#     ("Division", "Group", 'Class', 'Subclass'))
+# level = level_input if level_input else 'Class'
+
+# # Streamlit selectbox for filter criteria
+# filter_criteria = st.selectbox(
+#     "Filter last column by:",
+#     ("Both", "Y", "N")
+# )
+
+# # Get the selected DataFrame
+# selected_df = df_display[level_input]
+
+# # Get the name of the last column
+# last_column = selected_df.columns[-1]
+
+# # Apply filter based on user selection
+# if filter_criteria == "Y":
+#     filtered_df = selected_df[selected_df[last_column] == "Y"]
+# elif filter_criteria == "N":
+#     filtered_df = selected_df[selected_df[last_column] == "N"]
+# else:
+#     filtered_df = selected_df
+
+# # Display the filtered DataFrame with text wrapping and no truncation
+# st.dataframe(
+#     filtered_df.style.set_properties(**{
+#         'white-space': 'pre-wrap',
+#         'overflow-wrap': 'break-word',
+#     })
+# )
 
 # Visual Effects ### - https://docs.streamlit.io/develop/api-reference/status
 st.balloons() 
 
 st.sidebar.success("Explore our pages above ☝️")
 
-# Display plot in Streamlit
-st.pyplot(fig)
-
-
-df_display = {
-    "Section": Section_prediction_df,
-    "Division": Division_prediction_df,
-    "Group": Group_prediction_df,
-    "Class": Class_prediction_df,
-    "Subclass": Subclass_prediction_df
-}
-
-# Streamlit selectbox for user input
-level_input = st.selectbox(
-    "Data Inspection",
-    ("Section", "Division", "Group", "Class", "Subclass")
-)
-
-# Streamlit selectbox for filter criteria
-filter_criteria = st.selectbox(
-    "Filter last column by:",
-    ("Both", "Y", "N")
-)
-
-# Get the selected DataFrame
-selected_df = df_display[level_input]
-
-# Get the name of the last column
-last_column = selected_df.columns[-1]
-
-# Apply filter based on user selection
-if filter_criteria == "Y":
-    filtered_df = selected_df[selected_df[last_column] == "Y"]
-elif filter_criteria == "N":
-    filtered_df = selected_df[selected_df[last_column] == "N"]
-else:
-    filtered_df = selected_df
-
-# Display the filtered DataFrame with text wrapping and no truncation
-st.dataframe(
-    filtered_df.style.set_properties(**{
-        'white-space': 'pre-wrap',
-        'overflow-wrap': 'break-word',
-    })
-)
+import pdb;pdb.set_trace()
